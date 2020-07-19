88fb96a55dbd7e3bd71e20f59803209f31d14c01
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2020-07-19 18:09:23 +0200

    API Geolocation

---
+++ API Geolocation/scripts.js
@@ -0,0 +1,23 @@
+/*
+    https://developer.mozilla.org/es/docs/Web/API/NavigatorGeolocation/geolocation
+*/
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    const geolocation = navigator.geolocation
+
+    geolocation.getCurrentPosition(getPosition, error, options)
+})
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+}
+
+const getPosition = (position) => {
+    console.log(position)
+}
+
+const error = (error) => console.log(error)
